50b05375000581f2a116894efb10fb953c16e8f5
maya70 <maya70@vt.edu>
2019-11-01 11:03:54 +0000

    initial commit

---
+++ picanet_rscript.r
@@ -0,0 +1,125 @@
+library(readr)
+
+library(lubridate)
+
+library(parsedate)
+
+
+
+# Change the unit ID to match your unit
+
+source_file_path <-"z:/Qualdash/"
+
+dest_file_path <- "z:/Qualdash/picanet_admission/"
+
+dest_activity_path <- "z:/Qualdash/picanet_activity/"
+
+audit_filename <- "Admission.csv"
+
+#dateFormat <- "%d-%m-%y %H:%M"
+
+dateFormat <- "%d/%m/%Y %H:%M:%S"
+
+
+source = paste(source_file_path, audit_filename, sep='')
+
+# read raw admission data from its current location
+
+admission <- read_csv(source)
+
+
+
+# get years in data
+
+admdate <- as.Date(paste(admission$AdDate), dateFormat)
+
+adyear <- year(admdate)
+
+admission <- cbind(admission, adyear)
+
+
+
+allDates <- lapply(admission, function(x) !all(is.na(as.Date(as.character(x), format =dateFormat))))
+df <- as.data.frame(allDates)
+colnames(df) <- colnames(admission)
+dateFields <- df[which(df==TRUE)]
+
+
+
+# Unify date formats to ISO format
+
+for(col in colnames(admission)){
+  
+  if(col %in% colnames(dateFields)){
+    
+    vector <- admission[col]
+    
+    temp <- lapply(vector, function(x) as.POSIXlt(x, format=dateFormat))
+    
+    admission[col] <- temp
+  }
+}
+
+# break it into seperate files for individual years
+  # and store the new files in the picanet folder 
+for(year in unique(admission$adyear)){
+  tmp = subset(admission, adyear == year)     
+  fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
+  write.csv(tmp, fn, row.names = FALSE)
+}
+
+
+########
+yfn = paste(dest_file_path ,'avail_years.csv', sep='' )
+# read in values already in avail_years
+av <- read_csv(yfn)
+for(year in unique(admission$adyear)){
+  if(!(year %in% av$x))
+    temp <- c(av$x, year)
+}
+write.csv(temp, yfn, row.names = FALSE)
+
+
+# read raw activity data
+source = paste(source_file_path, "Dailyinterventions.csv", sep='')
+activity <- read_csv(source)
+
+# split activity file by year of admission
+myvars <- c("adyear", "EventID")
+newdata <- admission[myvars]
+df <- merge(newdata, activity, by='EventID')
+
+# get the latest year in your data
+#maxyear <- max(admission$adyear, na.rm = TRUE)
+
+for(year in unique(df$adyear)){
+  # read the admission records for the current year alone
+  fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
+  cur_adm <- read.csv(fn)
+  # merge the activity file
+  M <- merge(cur_adm, activity, by=c('EventID'), all.x=T)
+  
+  # select only relevant columns for QualDash
+  d = data.frame(M$EventID, M$AdDate, M$PccHrg)
+  colnames(d) <- c('EventID', 'adDate', 'hrggroup')
+  
+  for(level in unique(d$hrggroup)){
+    admission[, toString(level) ] <- 0
+  }
+  
+  for(row in 1:nrow(d)){
+    id <- d$EventID[row] 
+    level <- d$hrggroup[row]
+    lev <- toString(level[1])
+    admission[which(admission$EventID == id), lev] <- 1 + admission[which(admission$EventID == id), lev]
+    
+  }
+  tmp = subset(admission, adyear == year)     
+  fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
+  write.csv(tmp, fn, row.names = FALSE)
+  
+  fn = paste(dest_activity_path, 'shortactiv' , gsub(' ','', year), '.csv', sep='' )
+  write.csv(d, fn)
+}
+
+
