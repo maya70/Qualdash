5733fe6567610dce4bb51a20f768e49ec4a88500
maya70 <maya70@vt.edu>
2019-11-01 11:56:33 +0000

    Updated MINAP R script and session logs from 18 Oct visit

--- MINAP_rscript.r
+++ MINAP_rscript.r
@@ -1,34 +1,40 @@
-library(readr)
+# read raw admission data from its current location
+ library(readr)
  library(lubridate)
  library(parsedate)
 
-     source_file_path <- "G:/CIO Team/QualDash/"
-     dest_file_path <- "G:/CIO Team/QualDash/minap_admission/"
-     #dateFormat <- "%d-%m-%y %H:%M"
-     dateFormat <- "%d/%m/%Y %H:%M"
-     audit_filename <- "24062019 MINAPWithHeaders3Years.csv"
+ source_file_path <- "P:/Cardiology/Research/MINAP/Qualdash Test/"
+ dest_file_path <- "\\/VMRS-QUALDAS01/qualdash/home/data/minap_admission/"
+#dest_file_path <- "P:/Cardiology/Research/MINAP/Qualdash Test/minap_admission/"
+# source_file_path <-"z:/Qualdash/"
+ #dest_file_path <- "z:/Qualdash/minap_admission/"
+ 
+ #dateFormat <- "%d-%m-%y %H:%M"
+ #dateFormat <- "%Y-%m-%d %H:%M:%S"
+ dateFormat <- "%d-%B-%y"
+ audit_filename <- "MINAP_QD_upload.csv"
  
  source = paste(source_file_path, audit_filename, sep='')
  madmission <- read_csv(source)
 
  # get years in data
- admdate <- as.Date(madmission$`3.06 ArrivalAtHospital`, format=dateFormat)
+ admdate <- as.Date(madmission$`Arrival at hospital`, format=dateFormat)
  adyear <- year(admdate)
  madmission <- cbind(madmission, adyear)
 
 # Select all columns with Date data type
-
-allDates <- lapply(madmission, function(x) !all(is.na(as.Date(as.character(x), format =dateFormat))))
-df <- as.data.frame(allDates)
-colnames(df) <- colnames(madmission)
-dateFields <- df[which(df==TRUE)]
+ allDates <- lapply(madmission, function(x) !all(is.na(as.Date(as.character(x), format = dateFormat))))
+ df <- as.data.frame(allDates)
+ colnames(df) <- colnames(madmission)
+ dateFields <- df[which(df==TRUE)]
+ 
 
 # Unify date formats to ISO format 
 for(col in colnames(madmission)){
     if(col %in% colnames(dateFields)){
-     vector <- madmission[col]
-     temp <- lapply(vector, function(x) as.POSIXlt(x, format=dateFormat))
-     madmission[col] <- temp
+    	vector <- madmission[col]
+    	temp <- lapply(vector, function(x) as.POSIXlt(x, format=dateFormat))
+    	madmission[col] <- temp
 
     }
 }
@@ -44,28 +50,26 @@ dateFields2 <- df2[which(df2==TRUE)]
 # Unify date formats to ISO format 
 for(col in colnames(madmission)){
     if(col %in% colnames(dateFields2)){
-     vector <- madmission[col]
-     temp <- lapply(vector, function(x) as.POSIXlt(x, format=dateFormat))
-     madmission[col] <- temp
+    	vector <- madmission[col]
+    	temp <- lapply(vector, function(x) as.POSIXlt(x, format=dateFormat))
+    	madmission[col] <- temp
 
     }
 }
 
 
 # Derived columns
-v427 <- madmission$`4.27 DischargedOnThieno` == '1. Yes'
-v431 <- madmission$`4.27 DischargedOnThieno` == '1. Yes'
-
+v427 <- madmission$`Thienopyridine_inhibitor_disch` == '1. Yes'
+v431 <- madmission$`Discharged on Ticagrelor` == '1. Yes'
 madmission$P2Y12 <- as.numeric(v431 | v427)
 
-#TODO: update the following lines with the right field names 
-arr <- as.Date(madmission$`3.06 Date/time arrival at hospital`, format="%d/%m/%Y %H:%M")
-rep <- as.Date(madmission$`3.09 Date/time of reperfusion treatment`, format = dateFormat)
+arr <- as.Date(madmission$`Arrival at hospital`,  format="%d-%B-%y")
+rep <- as.Date(madmission$`Reperfusion treatment`)  
 dtb <- rep - arr
 madmission$dtb <- as.numeric(dtb)
 
-angio <- as.Date(madmission$`4.18 Local angio date`, format = dateFormat)
-dta <-  angio - arr
+angio <- as.Date(madmission$`Date angio performed`, format="%d-%B-%y")
+dta <- angio - arr
 madmission$dta <- as.numeric(dta)
 dtaH <- as.numeric(dta) / 60
 madmission$dtaTarget <- as.numeric(dtaH < 72.0)
@@ -82,4 +86,10 @@ madmission$dtaNoTarget <- as.numeric(dtaH > 72.0)
  }
 
  yfn = paste(dest_file_path, 'avail_years.csv', sep='' )
- write.csv(unique(adyear), yfn, row.names = FALSE)
+ av <- read_csv(yfn)
+ temp <- av$x
+ for(year in unique(madmission$adyear)){
+   if(!(year %in% av$x))
+     temp <- c(av$x, year)
+ }
+ write.csv(temp, yfn, row.names = FALSE)
