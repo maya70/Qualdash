1d976ae16ea50d851e469204e85108e352decb31
maya70 <maya70@vt.edu>
2019-11-07 09:13:30 +0000

    Visit on 11 Oct.: updated picanet_rscript

--- R/picanet_rscript.r
+++ R/picanet_rscript.r
@@ -1,144 +1,64 @@
-<<<<<<< HEAD
- library(readr)
- library(lubridate)
- library(parsedate)
-
-source_file_path <- "//xCMMC.nhs.uk/UserData$/Redir/Taric.Sheikh/Documents/QualDash/"
- dest_file_path <- "C:/Bitnami/wampstack-7.3.6-2/apache2/htdocs/qualdash/home/data/picanet_admission/"
- dest_activity_path <- "C:/Bitnami/wampstack-7.3.6-2/apache2/htdocs/qualdash/home/data/picanet_activity/"
- 
- audit_filename <- "admission.csv"
- #dateFormat <- "%d-%m-%y %H:%M"
- dateFormat <- "%d/%m/%Y %H:%M:%S"
- 
- 
- source = paste(source_file_path, audit_filename, sep='')
-# read raw admission data from its current location
- admission <- read_csv(source)
- #admission <- subset(admission, PicuOrg == unitID)
-
-# get years in data
- admdate <- as.Date(paste(admission$AdDate), dateFormat)
- adyear <- year(admdate)
- admission <- cbind(admission, adyear)
-=======
 library(readr)
-
 library(lubridate)
-
 library(parsedate)
 
+source_file_path <- "C:/Bitnami/wampstack-7.1.29-0/apache2/htdocs/qualdash/data_src/"
+dest_file_path <- "C:/Bitnami/wampstack-7.1.29-0/apache2/htdocs/qualdash/home/data/picanet_admission/"
+dest_activity_path <- "C:/Bitnami/wampstack-7.1.29-0/apache2/htdocs/qualdash/home/data/picanet_activity/"
 
-
-# Change the unit ID to match your unit
-
-source_file_path <-"z:/Qualdash/"
-
-dest_file_path <- "z:/Qualdash/picanet_admission/"
-
-dest_activity_path <- "z:/Qualdash/picanet_activity/"
-
-audit_filename <- "Admission.csv"
-
+audit_filename <- "admission.csv"
 #dateFormat <- "%d-%m-%y %H:%M"
-
 dateFormat <- "%d/%m/%Y %H:%M:%S"
 
 
 source = paste(source_file_path, audit_filename, sep='')
-
 # read raw admission data from its current location
-
 admission <- read_csv(source)
-
-
+#admission <- subset(admission, PicuOrg == unitID)
 
 # get years in data
-
 admdate <- as.Date(paste(admission$AdDate), dateFormat)
-
 adyear <- year(admdate)
-
-admission <- cbind(admission, adyear, invVentEncounter =0)
-
-
->>>>>>> 3d656f5772f9c13a9e70ba1182f3305d98c492d1
+admission <- cbind(admission, adyear)
 
 allDates <- lapply(admission, function(x) !all(is.na(as.Date(as.character(x), format =dateFormat))))
 df <- as.data.frame(allDates)
 colnames(df) <- colnames(admission)
 dateFields <- df[which(df==TRUE)]
 
-<<<<<<< HEAD
 # Unify date formats to ISO format 
 for(col in colnames(admission)){
-    if(col %in% colnames(dateFields)){
-         vector <- admission[col]
-         temp <- lapply(vector, function(x) as.POSIXlt(x, format=dateFormat))
-         admission[col] <- temp
-         
-     }
- }
-
-# break it into separate files for individual years
- # and store the new files in the picanet folder under documnt root 
- for(year in unique(admission$adyear)){
-     tmp = subset(admission, adyear == year)     
-     fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
-    write.csv(tmp, fn, row.names = FALSE)
- }
-=======
-
-
-# Unify date formats to ISO format
-
-for(col in colnames(admission)){
-  
   if(col %in% colnames(dateFields)){
-    
     vector <- admission[col]
-    
     temp <- lapply(vector, function(x) as.POSIXlt(x, format=dateFormat))
-    
     admission[col] <- temp
+    
   }
 }
 
-# break it into seperate files for individual years
-  # and store the new files in the picanet folder 
+# break it into separate files for individual years
+# and store the new files in the picanet folder under documnt root 
 for(year in unique(admission$adyear)){
   tmp = subset(admission, adyear == year)     
   fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
   write.csv(tmp, fn, row.names = FALSE)
 }
->>>>>>> 3d656f5772f9c13a9e70ba1182f3305d98c492d1
 
 
 ########
 yfn = paste(dest_file_path ,'avail_years.csv', sep='' )
 # read in values already in avail_years
 av <- read_csv(yfn)
-<<<<<<< HEAD
-for(year in unique(admission$adyear)){
-  if(!(year %in% av$x))
-         temp <- c(av$x, year)
-    }
-=======
 temp <- av$x
 for(year in unique(admission$adyear)){
   if(!(year %in% av$x))
     temp <- c(av$x, year)
 }
->>>>>>> 3d656f5772f9c13a9e70ba1182f3305d98c492d1
 write.csv(temp, yfn, row.names = FALSE)
 
 
 # read raw activity data
-<<<<<<< HEAD
 source = paste(source_file_path, "activity.csv", sep='')
-=======
-source = paste(source_file_path, "dailyinterventions.csv", sep='')
->>>>>>> 3d656f5772f9c13a9e70ba1182f3305d98c492d1
 activity <- read_csv(source)
 
 # split activity file by year of admission
@@ -150,39 +70,6 @@ df <- merge(newdata, activity, by='EventID')
 #maxyear <- max(admission$adyear, na.rm = TRUE)
 
 for(year in unique(df$adyear)){
-<<<<<<< HEAD
-    # read the admission records for the current year alone
-    fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
-    cur_adm <- read.csv(fn)
-    # merge the activity file
-    M <- merge(cur_adm, activity, by=c('EventID'), all.x=T)
-    
-    # select only relevant columns for QualDash
-    d = data.frame(M$EventID, M$AdDate, M$PccHrg)
-    colnames(d) <- c('EventID', 'adDate', 'hrggroup')
-    
-    for(level in unique(d$hrggroup)){
-        admission[, toString(level) ] <- 0
-    }
-    
-    for(row in 1:nrow(d)){
-        id <- d$EventID[row] 
-        level <- d$hrggroup[row]
-        lev <- toString(level[1])
-        admission[which(admission$EventID == id), lev] <- 1 + admission[which(admission$EventID == id), lev]
-        
-    }
-    tmp = subset(admission, adyear == year)     
-    fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
-    write.csv(tmp, fn, row.names = FALSE)
-    
-    fn = paste(dest_activity_path, 'shortactiv' , gsub(' ','', year), '.csv', sep='' )
-    write.csv(d, fn)
-}
-
-
-
-=======
   # read the admission records for the current year alone
   fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
   cur_adm <- read.csv(fn)
@@ -190,32 +77,18 @@ for(year in unique(df$adyear)){
   M <- merge(cur_adm, activity, by=c('EventID'), all.x=T)
   
   # select only relevant columns for QualDash
-  d = data.frame(M$EventID, M$ActivityMonth , M$ActivityDate , M$AdDate,   M$ActivityDate, M$PccHrg, M$InvVentET, M$InvVentTT)
-  colnames(d) <- c('EventID', 'ActivityMonth' ,'ActivityDate' , 'adDate', 'invVentStart', 'hrggroup', 'InvVentET', 'InvVentTT')
+  d = data.frame(M$EventID, M$AdDate, M$PccHrg)
+  colnames(d) <- c('EventID', 'adDate', 'hrggroup')
   
   for(level in unique(d$hrggroup)){
     admission[, toString(level) ] <- 0
   }
   
-  ventEvt <- d$InvVentET | d$InvVentTT
-  count = 0
   for(row in 1:nrow(d)){
     id <- d$EventID[row] 
     level <- d$hrggroup[row]
     lev <- toString(level[1])
-    # record the number of days at dependency level lev
     admission[which(admission$EventID == id), lev] <- 1 + admission[which(admission$EventID == id), lev]
-    
-    # record the start date of invasive ventilation for this admission
-    if(ventEvt[row]){
-      count <- count + 1
-      # is this the first time of invVent?
-      if(admission[which(admission$EventID == id), 'invVentEncounter'] == 0){
-        admission[which(admission$EventID == id), 'invVentEncounter'] <- 1
-        admission[which(admission$EventID == id), 'invVentStart'] <- d$ActivityDate[row]
-      }
-      
-    }
     
   }
   tmp = subset(admission, adyear == year)     
@@ -227,4 +100,4 @@ for(year in unique(df$adyear)){
 }
 
 
->>>>>>> 3d656f5772f9c13a9e70ba1182f3305d98c492d1
+
